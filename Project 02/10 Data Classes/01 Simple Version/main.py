d3212bff3ce237d3239d9f2f76001a6c03ed8649
Khosro HosseinAbadi <khosro.h2011@gmail.com>
2023-12-06 17:32:54 +0330

    Update content

--- Project 02/10 Data Classes/01 Simple Version/main.py
+++ Project 02/10 Data Classes/01 Simple Version/main.py
@@ -1,6 +1,7 @@
 import random
 import string
 
+
 def generate_id() -> str:
     return "".join(random.choices(string.ascii_uppercase, k=12))
 
@@ -14,12 +15,12 @@ class Person:
 
     def __str__(self):
         return f"name:{self.name}, age:{self.age}, address: {self.address}, phone number: {self.phone}"
-    
+
 
 def main() -> None:
     person = Person("Khosro", 31, "Iran Mashhad", 9357896541)
     print(person)
 
 
-if __name__ == "__main__" :
+if __name__ == "__main__":
     main()
